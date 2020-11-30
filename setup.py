9845fee767772b0a5440bed1b6cc304cc704ec14
Mario Lozano <riolp9991@gmail.com>
2020-11-30 10:22:15 -0500

    Creating Pip Settings

---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="alphorder",  # Replace with your own username
+    version="0.0.1",
+    author="riolp9991",
+    author_email="riolp9991@gmail.com",
+    description="Sort a folder's content with just one line of code",
+    long_description="",
+    license="MIT",
+    long_description_content_type="text/markdown",
+    url="https://github.com/riolp9991/alphorder",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
